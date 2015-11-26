be85a6b50f152a36142715a8fad95e9334e91a3f
Alireza Molaee <alirezamolaii@gmail.com>
2015-11-26 19:51:04 +0330

    update docs

--- wuser/storage.py
+++ wuser/storage.py
@@ -3,7 +3,7 @@ from django.core.files.storage import FileSystemStorage
 
 class OverwriteStorage(FileSystemStorage):
     """
-    Returns same name for existing file and deletes existing file on save.
+    Returns same name for existing file and override existing file on save.
     """
     def _save(self, name, content):
         if self.exists(name):
@@ -11,4 +11,9 @@ class OverwriteStorage(FileSystemStorage):
         return super(OverwriteStorage, self)._save(name, content)
 
     def get_available_name(self, name):
+        """
+        Override FileSystemStorage default behavior of giving unique filename
+        This will override previously uploaded file, given same filename.
+        Important: Do not change this.
+        """
         return name
