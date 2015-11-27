6ee243c736cb6b7fb5b50689348e98c500c90fa5
mhhg <hosseinhhg@hotmail.com>
2015-11-27 18:10:57 +0330

    Fix indentation.

--- wuser/views.py
+++ wuser/views.py
@@ -9,10 +9,9 @@ from django.utils.translation import ugettext_lazy as _
 def confirm_email(request, id, key):
     user = get_object_or_404(User, pk=id)
     if user.is_confirmed:
-        return render(request, template_name='admin/base_site.html',
-                      context={'messages': [
-                          _("Your email: {} has been already confirmed!".format(user.email))
-                      ], 'is_popup': True})
+        return render(request, template_name='admin/base_site.html', context={
+            'messages': [_("Your email: {} has been already confirmed!"
+                           .format(user.email))], 'is_popup': True})
     else:
         try:
             user.confirm_email(key, save=False)
