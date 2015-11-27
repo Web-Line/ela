f82dc7755c41ecb4ca4b2ac3d784c4a32187cc80
Alireza Molaee <alirezamolaii@gmail.com>
2015-11-27 19:34:09 +0330

    Change  id parameter to user_id

--- wuser/urls.py
+++ wuser/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^confirm-email/user(?P<id>[0-9]+)/(?P<key>[0-9]+)$',
+    url(r'^confirm-email/user(?P<user_id>[0-9]+)/(?P<key>[0-9]+)$',
         views.confirm_email, name='confirm-email'),
     
 ]
