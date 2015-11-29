7326bd27bcab9cb967868b6fc854166fe01353eb
mhhg <hosseinhhg@hotmail.com>
2015-11-29 09:17:55 +0330

    rolepermissions MUST be after django.contrib.admin in INSTALLED_APPS

--- ela/settings.py
+++ ela/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'suit',
-    'wuser',
-    'account',
-    'rolepermissions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wuser',
+    'account',
+    'rolepermissions',
 )
 
 MIDDLEWARE_CLASSES = (
