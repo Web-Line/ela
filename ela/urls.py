09d07ab4a2cac8cab5ef4a3bc2c5551105b26b95
mhhg <hosseinhhg@hotmail.com>
2015-11-29 12:07:03 +0330

    Remove email confirmation future from wuser

--- ela/urls.py
+++ ela/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-import wuser.urls as wsuser_urls
+# import wuser.urls as wsuser_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^authenticate/', include(wsuser_urls)),
+    # url(r'^authenticate/', include(wsuser_urls)),
 ]
--- wuser/views.py
+++ wuser/views.py
@@ -1,24 +1,24 @@
-from django.shortcuts import render
-from wuser.models import User
-from django.shortcuts import get_object_or_404
-from django.core.exceptions import PermissionDenied
-from django.utils.translation import ugettext_lazy as _
-
-
-# Create your views here.
-def confirm_email(request, user_id, key):
-    """
-     get user and confirmed_key to confirm and activate user (is_active)
-    """
-    user = get_object_or_404(User, pk=user_id)
-    if user.is_confirmed:
-        return render(request, template_name='admin/base_site.html', context={
-            'messages': [_("Your email: {} has been already confirmed!"
-                           .format(user.email))], 'is_popup': True})
-    else:
-        try:
-            user.confirm_email(key, save=False)
-        except:
-            raise PermissionDenied
-        user.is_active = True
-        user.save()
+# from django.shortcuts import render
+# from wuser.models import User
+# from django.shortcuts import get_object_or_404
+# from django.core.exceptions import PermissionDenied
+# from django.utils.translation import ugettext_lazy as _
+#
+#
+# # Create your views here.
+# def confirm_email(request, user_id, key):
+#     """
+#      get user and confirmed_key to confirm and activate user (is_active)
+#     """
+#     user = get_object_or_404(User, pk=user_id)
+#     if user.is_confirmed:
+#         return render(request, template_name='admin/base_site.html', context={
+#             'messages': [_("Your email: {} has been already confirmed!"
+#                            .format(user.email))], 'is_popup': True})
+#     else:
+#         try:
+#             user.confirm_email(key, save=False)
+#         except:
+#             raise PermissionDenied
+#         user.is_active = True
+#         user.save()
