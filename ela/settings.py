1959e78004fbd7d450c729b21743cdf3cfaf05e2
mhhg <hosseinhhg@hotmail.com>
2015-11-29 08:29:25 +0330

    Introduce usage of django-user-account

--- ela/settings.py
+++ ela/settings.py
@@ -31,15 +31,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'suit',
+    'account',
+    'rolepermissions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'simple_email_confirmation',
-    'wuser',
-    'rolepermissions',
 )
 
 MIDDLEWARE_CLASSES = (
