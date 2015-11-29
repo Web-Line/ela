4ed1ac4d0f095798a01f87df613829f59a4a2b17
mhhg <hosseinhhg@hotmail.com>
2015-11-29 12:08:30 +0330

    Support __unicode__ for python 3 Fix indentation Fix some doc Clear email confirmation feature

--- wuser/models.py
+++ wuser/models.py
@@ -1,15 +1,14 @@
 import os
-
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-    PermissionsMixin, Group
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.utils.translation import ugettext_lazy as _
 from django.utils.html import format_html
 from django.utils import timezone
 from django.conf import settings
-#from simple_email_confirmation import SimpleEmailConfirmationUserMixin
+# from simple_email_confirmation import SimpleEmailConfirmationUserMixin
 
 from wuser.storage import OverwriteStorage
 
@@ -28,75 +27,76 @@ class PathAndRename(object):
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, national_id, first_name, last_name, email, password=None):
+    def create_user(self, national_id, first_name, last_name, email,
+                    password=None):
         """
         Creates and saves a User with the given email, date of
         birth and password.
         """
         if not national_id:
-            raise ValueError('Users must set national id')
+            raise ValueError('Users must provide a national id')
 
-        user = self.model(
-            national_id=national_id,
-            first_name=first_name,
-            last_name=last_name,
-            email=email
-        )
+        user = self.model(national_id=national_id, first_name=first_name,
+                          last_name=last_name, email=email)
 
         user.set_password(password)
         user.is_admin = False
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, national_id, first_name, last_name, email, password):
+    def create_superuser(self, national_id, first_name, last_name, email,
+                         password):
         """
         Creates and saves a superuser with the given email, date of
         birth and password.
         """
-        user = self.create_user(national_id,
-                                first_name=first_name,
-                                last_name=last_name,
-                                email=email,
-                                password=password
-                                )
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
+        user = self.create_user(national_id, first_name=first_name,
+                                last_name=last_name, email=email,
+                                password=password)
+        user.is_admin, user.is_staff, user.is_superuser  = True, True, True
         user.save(using=self._db)
-        user.confirm_email(user.confirmation_key)
+        # user.confirm_email(user.confirmation_key)
         return user
 
 
 path_and_rename = PathAndRename("avatars")
 
 
-def kart_meli(value):
+def national_id_validator(value):
     """
-    checking national_id correct
+    Check if national_id is correct.
     :param value: national_id
     :type value: str
     :raises: ValidationError
     :returns: None
     """
+    # We don't need validation when DEBUG is True.
+    if settings.DEBUG:
+        return
+
     if len(value) != 10:
-        raise ValidationError(_('National ID most have 10 number'))
+        raise ValidationError(_('National ID most contain exactly 10 digits'))
+
     numbers_list = []
     result = 0
     for chars in value:
         try:
             numbers_list.append(int(chars))
         except:
-            raise ValidationError(_('National ID can only be numbers'))
+            raise ValidationError(_('National ID must be numeric'))
+
     numbers_list = list(reversed(numbers_list))
-    for i in xrange(len(numbers_list)):
+    for i in range(len(numbers_list)):
         if i != 0:
             result += (i + 1) * numbers_list[i]
+
     if result % 11 < 2:
         control = result % 11
     else:
         control = 11 - (result % 11)
     if control != numbers_list[0]:
-        raise ValidationError(_('National ID is not correct'))
+        raise ValidationError(_('Invalid National ID, Please make sure provided'
+                                ' National ID is correct'))
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -105,10 +105,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     features.
     """
     national_id = models.CharField(_('National ID(username)'), max_length=10,
-                                   unique=True, validators=[kart_meli])
+                                   unique=True,
+                                   validators=[national_id_validator])
     first_name = models.CharField(_('first name'), max_length=30)
     last_name = models.CharField(_('last name'), max_length=30)
-    email = models.EmailField(_('email address'),)
+    email = models.EmailField(_('email address'), )
     photo = models.ImageField(_('profile image'), upload_to=path_and_rename,
                               storage=OverwriteStorage(), null=True,
                               blank=True)
@@ -137,8 +138,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         return self.first_name
 
-    @property
-    def pic(self):
+    def get_image_url(self):
         """
         :return: the url of image profile for user
         """
@@ -151,20 +151,18 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         :return:the thumbnail html for 80x80 picture
         """
-        return format_html(
-            '<img src="{}" class="img-thumbnail" width="80" height="80">',
-            self.pic)
+        return format_html('<img src="{}" class="img-thumbnail"'
+                           ' width="80" height="80">', self.get_image_url)
 
     get_pic_html.short_description = _("Thumbnail")
 
     thumbnail = property(get_pic_html)
 
-    def __str__(self):
-        return self.get_full_name
+    def __unicode__(self):
+        return self.get_full_name()
 
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
-
 
 # admin_user = User.objects.get(is_superuser=True)
