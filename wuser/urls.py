b1dcb66442c008c0e074fdd978b2e4974a45411c
mhhg <hosseinhg@hotmail.com>
2015-12-01 20:41:18 +0330

    Initial pinax branch commit

--- wuser/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from wuser import views
-
-#
-# urlpatterns = [
-#     url(r'^confirm-email/user(?P<user_id>[0-9]+)/(?P<key>[0-9]+)$',
-#         views.confirm_email, name='confirm-email'),
-#
-# ]
