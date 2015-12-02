6731ffca6bdd20f648cd2ddbbd6f2fa707ce5ef5
Alireza Molaee <alirezamolaii@gmail.com>
2015-12-02 18:57:31 +0330

    Add UserProfile

--- user_account/models.py
+++ user_account/models.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.db import models
 
 
-# class UserProfile(models):
-#     fathers_name = models.CharField(max_length=30)
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    fathers_name = models.CharField(max_length=30)
+    birthdate = models.DateField()
