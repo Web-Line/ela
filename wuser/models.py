96953e27b18e5f8e401f6ce7c38f29fcf643d560
mhhg <hosseinhg@hotmail.com>
2015-12-03 09:03:54 +0330

    1. Move UserProfile class into wuser/models.py 2. Changed some lower case to upper case help messages. 3. Better style of property vs functions.

--- wuser/models.py
+++ wuser/models.py
@@ -1,6 +1,5 @@
 import os
 from django.db import models
-from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
                                         PermissionsMixin, Permission)
@@ -9,8 +8,8 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.html import format_html
 from django.utils import timezone
 from django.conf import settings
-
 from wuser.storage import OverwriteStorage
+from wuser.validators import national_id_validator
 
 
 class Group(MasterGroup):
@@ -57,7 +56,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(national_id, first_name=first_name,
                                 last_name=last_name, email=email,
                                 password=password)
-        user.is_admin, user.is_staff, user.is_superuser  = True, True, True
+        user.is_admin, user.is_staff, user.is_superuser = True, True, True
         user.save(using=self._db)
         # user.confirm_email(user.confirmation_key)
         return user
@@ -65,79 +64,6 @@ class UserManager(BaseUserManager):
 
 path_and_rename = PathAndRename("avatars")
 
-
-def national_id_validator(value):
-    """
-    Check if national_id is correct.
-    :param value: national_id
-    :type value: str
-    :raises: ValidationError
-    :returns: None
-    """
-    # We don't need validation when DEBUG is True.
-    if settings.DEBUG:
-        return
-
-    if len(value) != 10:
-        raise ValidationError(_('National ID most contain exactly 10 digits'))
-
-    numbers_list = []
-    result = 0
-    for chars in value:
-        try:
-            numbers_list.append(int(chars))
-        except:
-            raise ValidationError(_('National ID must be numeric'))
-
-    numbers_list = list(reversed(numbers_list))
-    for i in range(len(numbers_list)):
-        if i != 0:
-            result += (i + 1) * numbers_list[i]
-
-    if result % 11 < 2:
-        control = result % 11
-    else:
-        control = 11 - (result % 11)
-    if control != numbers_list[0]:
-        raise ValidationError(_('Invalid National ID, Please make sure provided'
-                                ' National ID is correct'))
-
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
-    fathers_name = models.CharField(_('fathers name'), max_length=30)
-    birthdate = models.DateField(_('birthdate'))
-    phone_number = models.CharField(_('phone number'), max_length=20,
-                                    null=True, blank=False)
-    home_phone = models.CharField(_('home phone'), max_length=20, null=True,
-                                  blank=True)
-    home_address = models.TextField(_('home address'), max_length=100,
-                                    null=True, blank=False)
-    MALE = 1
-    FEMALE = 2
-    GENDER_CHOICES = (
-        (MALE, 'Male'),
-        (FEMALE, 'Female'),
-    )
-    gender = models.IntegerField(_('gender'), choices=GENDER_CHOICES,
-                                 null=True, blank=False)
-    description = models.TextField(_('description'), null=True, blank=True,
-                                   max_length=150)
-    WEBSITE = 1
-    TRAKT = 2
-    FRIENDS = 3
-    OTHER = 4
-    ACQUAINTANCE_WAY_CHOICES = (
-        (WEBSITE, 'Website'),
-        (TRAKT, 'Trakt'),
-        (FRIENDS, 'Friends'),
-        (OTHER, 'Other')
-    )
-    acquaintance_way = models.IntegerField(_('witch connect you to ela'),
-                                           choices=ACQUAINTANCE_WAY_CHOICES,
-                                           default=WEBSITE, null=True,
-                                           blank=True)
 
 class User(AbstractBaseUser, PermissionsMixin):
     """
@@ -147,18 +73,18 @@ class User(AbstractBaseUser, PermissionsMixin):
     national_id = models.CharField(_('National ID(username)'), max_length=10,
                                    unique=True,
                                    validators=[national_id_validator])
-    first_name = models.CharField(_('first name'), max_length=30)
-    last_name = models.CharField(_('last name'), max_length=30)
-    email = models.EmailField(_('email address'), )
-    photo = models.ImageField(_('profile image'), upload_to=path_and_rename,
-                              storage=OverwriteStorage(), null=True,
-                              blank=True)
-    is_staff = models.BooleanField(_('staff status'), default=False, help_text=
+    first_name = models.CharField(_('First Name'), max_length=30)
+    last_name = models.CharField(_('Last Name'), max_length=30)
+    email = models.EmailField(_('Email'), )
+    picture = models.ImageField(_('Profile Picture'), upload_to=path_and_rename,
+                                storage=OverwriteStorage(), null=True,
+                                blank=True)
+    is_staff = models.BooleanField(_('Staff Status'), default=False, help_text=
     _('Designates whether the user can log into staff site.'))
-    is_active = models.BooleanField(_('active'), default=True, help_text=
+    is_active = models.BooleanField(_('Active'), default=True, help_text=
     _('Designates whether this user should be treated as active. Unselect this '
       'instead of deleting accounts.'))
-    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+    date_joined = models.DateTimeField(_('Date Joined'), default=timezone.now)
 
     objects = UserManager()
 
@@ -171,8 +97,6 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
-    
-    full_name = property(get_full_name)
 
     def get_short_name(self):
         """
@@ -180,25 +104,28 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         return self.first_name
 
-    def get_image_url(self):
+    @property
+    def picture_url(self):
         """
         :return: the url of image profile for user
         """
-        if self.photo:
-            return self.photo.url
+        if self.picture:
+            return self.picture.url
         else:
             return os.path.join(settings.MEDIA_URL, 'avatars/0.png')
 
-    def get_pic_html(self):
+    @property
+    def thumbnail(self):
         """
         :return:the thumbnail html for 80x80 picture
         """
         return format_html('<img src="{}" class="img-thumbnail"'
                            ' width="80" height="80">', self.get_image_url)
 
-    get_pic_html.short_description = _("Thumbnail")
+    # thumbnail.short_description = _("Thumbnail")
 
-    thumbnail = property(get_pic_html)
+    full_name = property(get_full_name)
+    short_name = property(get_short_name)
 
     def __unicode__(self):
         return self.get_full_name()
@@ -207,4 +134,41 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name = _('user')
         verbose_name_plural = _('users')
 
+
 # admin_user = User.objects.get(is_superuser=True)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    fathers_name = models.CharField(_('Fathers Name'), max_length=30)
+    birth_date = models.DateField(_('Birth Date'))
+    phone_number = models.CharField(_('Phone Number'), max_length=20,
+                                    null=True, blank=False)
+    home_phone = models.CharField(_('Home Phone'), max_length=20, null=True,
+                                  blank=True)
+    home_address = models.TextField(_('Home Address'), max_length=100,
+                                    null=True, blank=False)
+    MALE = 1
+    FEMALE = 2
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    )
+    gender = models.IntegerField(_('Gender'), choices=GENDER_CHOICES,
+                                 null=True, blank=False)
+    description = models.TextField(_('Description'), null=True, blank=True,
+                                   max_length=150)
+    WEBSITE = 1
+    TRAKT = 2
+    FRIENDS = 3
+    OTHER = 4
+    ACQUAINTANCE_WAY_CHOICES = (
+        (WEBSITE, 'Website'),
+        (TRAKT, 'Trakt'),
+        (FRIENDS, 'Friends'),
+        (OTHER, 'Other')
+    )
+    acquaintance_way = models.IntegerField(
+        _('How did you find "ela"?'), choices=ACQUAINTANCE_WAY_CHOICES,
+        default=WEBSITE, null=True,
+        blank=True)
