f2c48caf645726d74a581b693d034d6fdf46c686
mhhg <hosseinhg@hotmail.com>
2015-12-03 09:07:45 +0330

    Add handle_user_logged_in()

--- wuser/signals.py
+++ wuser/signals.py
@@ -1,12 +1,16 @@
-# from django.dispatch import receiver
-# from django.db.models.signals import post_save
-#
-# from django.contrib.auth.models import User
-#
-# from mysite.profiles.models import UserProfile
-#
-#
+from django.dispatch import receiver
+from account.signals import user_logged_in
+from wuser.views import LoginView
+import logging
+
+logger = logging.getLogger("ela")
+
 # @receiver(post_save, sender=User)
 # def handle_user_save(sender, instance, created, **kwargs):
 #     if created:
 #         UserProfile.objects.create(user=instance)
+
+
+@receiver(user_logged_in, sender=LoginView)
+def handle_user_logged_in(sender, **kwargs):
+    logger.debug("kwargs={}".format(kwargs))
