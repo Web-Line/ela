62ef9e1be3717c2a37368c26bd5f05f484e769cd
mhhg <hosseinhg@hotmail.com>
2015-12-04 13:47:27 +0330

    Add national_id CharFields to SignupForm
    I don't know  the reason of username field existence on signup/ url
    We have to find out.

--- wuser/forms.py
+++ wuser/forms.py
@@ -4,28 +4,32 @@ from django.utils.translation import ugettext_lazy as _
 from wuser.models import UserProfile
 from django.core.validators import RegexValidator
 import account.forms
+from wuser.validators import national_id_validator
 
 
 class SignupForm(account.forms.SignupForm):
-    photo = forms.ImageField(required=False,)
+    national_id = forms.CharField(label=_('national ID (username)'),
+                                  max_length=10,
+                                  validators=[national_id_validator])
+    photo = forms.ImageField(required=False)
     first_name = forms.CharField(required=True, max_length=30)
     last_name = forms.CharField(required=True, max_length=30)
     fathers_name = forms.CharField()
     gender = forms.ChoiceField(label=_('gender'),
                                choices=UserProfile.GENDER_CHOICES,
                                required=True)
-    birthdate = forms.DateField(widget=SelectDateWidget(years=range(1910, 1991)))
-    phone_number = forms.CharField(label=_('phone number'),
-                                   validators=[RegexValidator(regex='^\d*$',
-                                                              message=_('not correct, only numbers'))],
-                                   max_length=20,
-                                   required=True)
-    home_phone = forms.CharField(label=_('home phone'),
-                                 validators=[RegexValidator(regex='^\d*$',
-                                                            message=_('not correct, only numbers'))],
-                                 max_length=20)
+    birth_date = forms.DateField(
+        widget=SelectDateWidget(years=range(1910, 1991)))
+    phone_number = forms.CharField(
+        label=_('phone number'), validators=[RegexValidator(
+            regex='^\d*$', message=_('not correct, only numbers'))],
+        max_length=20, required=True)
+    home_phone = forms.CharField(
+        label=_('home phone'), validators=[RegexValidator(
+            regex='^\d*$', message=_('not correct, only numbers'))],
+        max_length=20)
     home_address = forms.CharField(label=_('home address'), max_length=100)
-    acquaintance_way = forms.ChoiceField(label=_('acquaintance way'),
-                                         choices=UserProfile.ACQUAINTANCE_WAY_CHOICES,
-                                         required=True)
+    acquaintance_way = forms.ChoiceField(
+        label=_('acquaintance way'),
+        choices=UserProfile.ACQUAINTANCE_WAY_CHOICES, required=True)
     description = forms.CharField(label=_('description'), max_length=150)
