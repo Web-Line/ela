adcb453938e09fccb9f212afc3b08544e36125eb
mhhg <hosseinhg@hotmail.com>
2015-12-04 20:54:56 +0330

    Add logger into important events like usr/views.py/SignUpView create_user() and update_profile() ... to track log of those events.

--- usr/views.py
+++ usr/views.py
@@ -1,35 +1,9 @@
-# from django.shortcuts import render
-# from wuser.models import User
-# from django.shortcuts import get_object_or_404
-# from django.core.exceptions import PermissionDenied
-# from django.utils.translation import ugettext_lazy as _
-#
-#
-# # Create your views here.
-# def confirm_email(request, user_id, key):
-#     """
-#      get user and confirmed_key to confirm and activate user (is_active)
-#     """
-#     user = get_object_or_404(User, pk=user_id)
-#     if user.is_confirmed:
-#         return render(request, template_name='admin/base_site.html', context={
-#             'messages': [_("Your email: {} has been already confirmed!"
-#                            .format(user.email))], 'is_popup': True})
-#     else:
-#         try:
-#             user.confirm_email(key, save=False)
-#         except:
-#             raise PermissionDenied
-#         user.is_active = True
-#         user.save()
-
 from account.views import (SignupView as AccountSignupView,
                            LoginView as AccountLoginView)
 from usr.forms import SignupForm
 from usr.models import UserProfile, User
 from account import signals
 from pinax.notifications.models import send
-
 import logging
 
 logger = logging.getLogger("ela")
@@ -39,13 +13,18 @@ class SignupView(AccountSignupView):
     form_class = SignupForm
 
     def create_user(self, form, commit=True, **kwargs):
-        user = super(SignupView, self).create_user(form,  commit=False, **kwargs)
+        logger.debug(
+            "form='{}', commit='{}', kwargs='{}'".format(form, commit, kwargs))
+
+        user = super(SignupView, self).create_user(form, commit=False, **kwargs)
         user.national_id = form.cleaned_data['username']
         if commit:
             user.save()
         return user
 
     def update_profile(self, form):
+        logger.debug("form={}".format(form))
+
         UserProfile.objects.create(
             user=self.created_user,
             birth_date=form.cleaned_data["birth_date"],
@@ -53,6 +32,8 @@ class SignupView(AccountSignupView):
         )
 
     def after_signup(self, form):
+        logger.debut("form={}".format(form))
+
         self.update_profile(form)
         admin = User.objects.filter(is_superuser=True)
         # send([admin], "signup_user")
@@ -70,4 +51,5 @@ class LoginView(AccountLoginView):
 
     def after_login(self, form):
         logger.debug("form={}".format(form))
+
         signals.user_logged_in.send(sender=LoginView, user=form.user, form=form)
