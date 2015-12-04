fb2ba627aa43c21c0160d7e71ebbd8e497c70a51
mhhg <hosseinhg@hotmail.com>
2015-12-04 21:02:02 +0330

    Setup proxy models. (Student, Teacher, Supervisor)
    We need these proxied with appropriate managers to have ModelAdmin
    support and also have a cleaner syntax on foreign keys to specific
    user type.

---
+++ usr/proxy_models.py
@@ -0,0 +1,30 @@
+from django.utils.translation import ugettext_lazy as _
+from usr.managers import (StudentManager, TeacherManager, SupervisorManager)
+from usr.models import User
+
+
+class Student(User):
+    objects = StudentManager()
+
+    class Meta:
+        proxy = True
+        verbose_name = _("Student")
+        verbose_name_plural = _("Students")
+
+
+class Teacher(User):
+    objects = TeacherManager()
+
+    class Meta:
+        proxy = True
+        verbose_name = _("Teacher")
+        verbose_name_plural = _("Teachers")
+
+
+class Supervisor(User):
+    objects = SupervisorManager()
+
+    class Meta:
+        proxy = True
+        verbose_name = _("Supervisor")
+        verbose_name_plural = _("Supervisors")
