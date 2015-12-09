b603ccf29372731bb456c459d76955f357e391ea
mhhg <hosseinhg@hotmail.com>
2015-12-09 13:35:43 +0330

    Initial setup of AdminSites in ela/sites.py
    MainAdminSite class is a copy of django.contrib.admin.sites AdminSite
    class.
    
    main_admin_site = MainAdminSite(name="admin")
    student_admin_site = StudentAdminSite(name="student")
    teacher_admin_site = TeacherAdminSite(name="teacher")
    
    Admin sites for each user type. we have to improve them.

--- ela/urls.py
+++ ela/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
-from django.contrib import admin
-
+from ela.sites import (main_admin_site, student_admin_site, teacher_admin_site)
 
 urlpatterns = patterns(
     "",
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
-    url(r"^admin/", include(admin.site.urls)),
+    url(r"^admin/", include(main_admin_site.urls), name="admin"),
+    url(r"^student/", include(student_admin_site.urls), name="student"),
+    url(r"^teacher/", include(teacher_admin_site.urls), name="teacher"),
     url(r"^account/", include("usr.urls")),
     url(r"^account/", include("account.urls")),
     url(r"^notifications/", include("pinax.notifications.urls")),
