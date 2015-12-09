45b948cc2f602ec5293703c4e287be78bdbf900b
mhhg <hosseinhg@hotmail.com>
2015-12-09 13:52:34 +0330

    Update usr/conf.py
    Clean up a bit.
    Remove superuser addition on post_migrate signal of usr app, and
    moved it to ela/apps.py

--- usr/conf.py
+++ usr/conf.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
-from django.contrib.auth import get_user_model
 import logging
 
 logger = logging.getLogger("ela")
@@ -15,9 +14,12 @@ def create_notice_types(sender, **kwargs):
     if "pinax.notifications" in settings.INSTALLED_APPS:
         from pinax.notifications.models import NoticeType
         logger.debug("Creating notices for ela")
-        NoticeType.create("signup_user", _("User Signup"),
-                          _("an user join to ela"))
-    #     ... for more notice type
+        NoticeType.create(
+            "signup_user",
+            _("User Signup"),
+            _("an user join to ela")
+        )
+    # ... for more notice type
     else:
         logger.debug("Skipping creation of NoticeTypes as notification app not "
                      "found")
@@ -25,24 +27,19 @@ def create_notice_types(sender, **kwargs):
 
 def handle_post_migrate(sender, **kwargs):
     """
-    It sets up a superuser when DEBUG is True.
+    documentation is needed here.
     :param sender:
     :param kwargs:
     :return:
     """
-    logger.debug('Handling post_migrate signal, DEBUG={}, interactive={}'.format(
-        settings.DEBUG, kwargs['interactive']))
-    if settings.DEBUG and kwargs['interactive']:
-        get_user_model().objects.create_superuser(123, "arc", "Karami",
-                                                            "arc@arc.ir", 123)
-    #
+
     create_notice_types(sender)
 
 
-class WUserAppConfig(AppConfig):
+class UsrAppConfig(AppConfig):
     name = 'usr'
     verbose_name = _("Authentication")
 
     def ready(self):
-        post_migrate.connect(handle_post_migrate, sender=self)
+        post_migrate.connect(handle_post_migrate)
         from usr.signals import handle_user_logged_in
