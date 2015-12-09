6b4cb4092fdd14bff9739307614d5d8a243ba992
mhhg <hosseinhg@hotmail.com>
2015-12-09 13:17:27 +0330

    Setup roles from settings inside of ela/settings.py
    Now we have permissions for login based on settings:
    	LOGIN_STUDENT_ADMIN_SITE = 'login_student_admin_site'
    
    StudentAdminAuthenticationForm in its confirm_login_allowd() function
    checks wheather user has this permission or not, to let the user login.
    
    We could check for user role instead of a login permission, but i think
    this way is more flexible for us in development (think about additional
    features and additional permissions for those features) and admin/staff(s)
    to handle exceptional situatuins like having a user which needs to
    log into two different admin sites, but he/she has one role(group),
    They can simple assign the LOGIN_STUDENT_ADMIN_SITE to let him login
    into Student site, for example.

--- ela/roles.py
+++ ela/roles.py
@@ -3,18 +3,21 @@ from django.conf import settings
 
 
 class StudentRole(AbstractUserRole):
-    available_permissions = [
-        settings.LOGIN_STUDENT_ADMIN_SITE,
-    ]
+    role_name = settings.STUDENT_ROLE_GROUP_NAME
+    available_permissions = {
+        settings.LOGIN_STUDENT_ADMIN_SITE: True
+    }
 
 
 class TeacherRole(AbstractUserRole):
-    available_permissions = [
-        settings.LOGIN_TEACHER_ADMIN_SITE,
-    ]
+    role_name = settings.TEACHER_ROLE_GROUP_NAME
+    available_permissions = {
+        settings.LOGIN_TEACHER_ADMIN_SITE: True,
+    }
 
 
 class SupervisorRole(AbstractUserRole):
-    available_permissions = [
-        settings.LOGIN_SUPERVISOR_ADMIN_SITE,
-    ]
+    role_name = settings.SUPERVISOR_ROLE_GROUP_NAME
+    available_permissions = {
+        settings.LOGIN_SUPERVISOR_ADMIN_SITE: True,
+    }
